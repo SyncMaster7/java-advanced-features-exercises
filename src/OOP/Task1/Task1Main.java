37dfe6e851caf462777fb5c92d53057e3688856e
Kermo Keerma <32697937+SyncMaster7@users.noreply.github.com>
2023-03-04 10:55:18 +0200

    OOP - Task 1

---
+++ src/OOP/Task1/Task1Main.java
@@ -0,0 +1,10 @@
+package OOP.Task1;
+
+public class Task1Main {
+    public static void main(String[] args) {
+        Point2D twoPoint = new Point2D(2, 3);
+        System.out.println(twoPoint);
+        Point3D threePoint = new Point3D(2, 3, 4);
+        System.out.println(threePoint);
+    }
+}
